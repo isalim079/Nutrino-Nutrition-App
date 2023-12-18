5fa45ca126e86e0ac72064a53703b680d4a081cc
isalim079 <salimreza1025@gmail.com>
2023-12-18 14:59:16 +0600

    favicon and logo added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { ThemeProvider } from '@material-tailwind/react'
 import { RouterProvider } from 'react-router-dom'
+import { router } from './router/Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <RouterProvider></RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )

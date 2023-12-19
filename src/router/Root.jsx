e54619db0d71983147e983c5c4b4761ca01fa1f3
isalim079 <salimreza1025@gmail.com>
2023-12-19 19:42:38 +0600

    Root added

--- src/router/Root.jsx
+++ src/router/Root.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Navbar/Navbar";
+
 
 
 const Root = () => {
     return (
         <div>
+            <Navbar />
             <Outlet></Outlet>
         </div>
     );

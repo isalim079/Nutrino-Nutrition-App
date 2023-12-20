dfda97f6d65d441e95d462fe1210394b0c5dad4c
isalim079 <salimreza1025@gmail.com>
2023-12-20 14:40:29 +0600

    banner added with responsive carousel

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,12 +1,28 @@
+import image1 from "../../../assets/images/bannerImage/Fruits.jpg";
+import image2 from "../../../assets/images/bannerImage/Fruits-on-plate.jpg";
+import image3 from "../../../assets/images/bannerImage/leaf-fruits.jpg";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import image1 from "../../../assets/images/bannerImage/Fruits.jpg"
-import image2 from "../../../assets/images/bannerImage/Fruits-on-plate.jpg"
-import image3 from "../../../assets/images/bannerImage/leaf-fruits.jpg"
- 
 export function Banner() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <Carousel>
+                    <div>
+                        <img src={image1} />
+                        <p className="legend">Eat healthy</p>
+                    </div>
+                    <div>
+                        <img src={image2} />
+                        <p className="legend">Diet properly</p>
+                    </div>
+                    <div>
+                        <img src={image3} />
+                        <p className="legend">Lead a healthy life</p>
+                    </div>
+                </Carousel>
+            </div>
+        </div>
+    );
 }

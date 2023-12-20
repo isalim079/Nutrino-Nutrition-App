ab3ea585972c06cca627e6b229187c423b7323c2
isalim079 <salimreza1025@gmail.com>
2023-12-20 11:40:32 +0600

    page alignment done

--- src/router/ErrorPage.jsx
+++ src/router/ErrorPage.jsx
@@ -1,10 +1,21 @@
 import Lottie from "lottie-react";
-import errorPage from "../assets/errorPage/errorPage.json"
+import errorPage from "../assets/errorPage/errorPage.json";
 
 const ErrorPage = () => {
+
+    
+
     return (
         <div>
-            <Lottie animationData={errorPage} loop={true} />
+            <div className="border border-black h-screen flex justify-center items-center">
+                <div className="w-80">
+                    <Lottie animationData={errorPage} loop={true} />
+                    <div className="text-center text-green-900 ">
+                        <h1 className="text-8xl font-bold leading-[70px]">404</h1>
+                        <p className="text-lg">page not found</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

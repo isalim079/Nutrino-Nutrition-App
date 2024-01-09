b997773ca082faefa3e3a3cbdee84c46322a8b7d
isalim079 <salimreza1025@gmail.com>
2024-01-09 19:45:12 +0600

    dynamically image added in meal of the day section

--- src/pages/Home/FeatureContent/MealOfTheDay.jsx
+++ src/pages/Home/FeatureContent/MealOfTheDay.jsx
@@ -4,55 +4,43 @@ import dinner from "../../../assets/images/mealOfTheDay/Dinner.png";
 import { FaCaretLeft, FaCaretRight } from "react-icons/fa";
 import { useState } from "react";
 
-const images = [breakFast, lunch, dinner]
+const images = [breakFast, lunch, dinner];
 
 const MealOfTheDay = () => {
-
-    const [currentSlide, setCurrentSlide] = useState(0)
+    const [currentSlide, setCurrentSlide] = useState(0);
 
     const goNextSlide = () => {
-        setCurrentSlide(prevSlide => (prevSlide === images.length - 1 ? 0 : prevSlide + 1))
-    }
+        setCurrentSlide((prevSlide) =>
+            prevSlide === images.length - 1 ? 0 : prevSlide + 1
+        );
+    };
 
     const goPrevSlide = () => {
-        setCurrentSlide(prevSlide => (prevSlide === 0 ? images.length - 1 : prevSlide - 1))
-    }
+        setCurrentSlide((prevSlide) =>
+            prevSlide === 0 ? images.length - 1 : prevSlide - 1
+        );
+    };
 
     return (
         <div>
-           <div className="w-96 relative">
-                {/* 1st card */}
-                {/* <div id="slide1" className="relative border border-black">
-                    <img className="w-60" src={breakFast} alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide3"><button className="border"><FaCaretLeft className="text-2xl"/></button></a>
-                        <a href="#slide2"><button><FaCaretRight className="text-2xl"/></button></a>
+            <div className="w-96 relative">
+                <div className="border border-black relative bg-green-100">
+                    <div className="flex justify-center p-5 drop-shadow-md">
+                        <img
+                            className="w-60"
+                            src={images[currentSlide]}
+                            alt=""
+                        />
                     </div>
-                </div> */}
-                {/* 2nd card */}
-                {/* <div id="slide2" className="relative border border-black">
-                    <img className="w-60" src={lunch} alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1"><button><FaCaretLeft className="text-2xl"/></button></a>
-                        <a href="#slide3"><button><FaCaretRight className="text-2xl"/></button></a>
+                    <div className="absolute flex justify-between items-center transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <button className="" onClick={goPrevSlide}>
+                            <FaCaretLeft className="text-3xl text-green-200 bg-green-900 rounded-md pr-1" />
+                        </button>
+                        <button className="" onClick={goNextSlide}>
+                            <FaCaretRight className="text-3xl text-green-200 bg-green-900 rounded-md pl-1" />
+                        </button>
                     </div>
-                </div> */}
-                {/* 3rd card */}
-                {/* <div id="slide3" className="relative border border-black">
-                    <img className="w-60" src={dinner} alt="" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2"><button><FaCaretLeft className="text-2xl"/></button></a>
-                        <a href="#slide1"><button><FaCaretRight className="text-2xl"/></button></a>
-                    </div>
-                </div> */}
-
-<div className="border border-black relative">
-        <img className="w-60" src={images[currentSlide]} alt="" />
-        <div className="absolute flex justify-between items-center transform -translate-y-1/2 left-5 right-5 top-1/2">
-          <button onClick={goPrevSlide}><FaCaretLeft className="text-2xl" /></button>
-          <button onClick={goNextSlide}><FaCaretRight className="text-2xl" /></button>
-        </div>
-      </div>
+                </div>
             </div>
         </div>
     );

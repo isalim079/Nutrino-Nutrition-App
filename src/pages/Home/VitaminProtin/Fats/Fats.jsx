6bdfa82a7da5ed475ed31c95614faacadd1fd967
isalim079 <salimreza1025@gmail.com>
2024-01-11 23:00:05 +0600

    fats section added

--- src/pages/Home/VitaminProtin/Fats/Fats.jsx
+++ src/pages/Home/VitaminProtin/Fats/Fats.jsx
@@ -1,9 +1,36 @@
-
+import fatImage from "../../../../assets/images/sixNutritions/fat.png";
 
 const Fats = () => {
     return (
-        <div>
-            
+        <div className="bg-green-100 p-10 rounded-lg px-28 mt-10">
+            <div className="flex justify-between items-center">
+                <div className="flex-1">
+                    <img className="w-80" src={fatImage} alt="" />
+                </div>
+
+                <div className="border-l-4 pl-10 border-green-800 flex-1">
+                    <div>
+                        <h1 className="font-comicSans text-left font-semibold text-4xl">
+                            Fats
+                        </h1>
+                        <p className="mt-3 font-comicSans text-justify text-lg">
+                            Fats, often misunderstood, are crucial for optimal
+                            health, serving as a concentrated source of energy
+                            and playing key roles in essential bodily functions.
+                            Explore the diverse world of fats, including healthy
+                            unsaturated fats found in avocados, nuts, and olive
+                            oil, as well as omega-3 fatty acids abundant in
+                            fatty fish like salmon. Understand the importance of
+                            balancing saturated and trans fats while
+                            appreciating their role in hormone production and
+                            nutrient absorption. Embrace a well-rounded diet
+                            that incorporates a variety of fats to support
+                            overall wellness and maintain a harmonious
+                            equilibrium within the body.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };

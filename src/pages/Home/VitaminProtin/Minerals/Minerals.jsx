98cb2003221aeee6d35ee3c075a4f146b9880f65
isalim079 <salimreza1025@gmail.com>
2024-01-11 23:13:25 +0600

    minerals added

--- src/pages/Home/VitaminProtin/Minerals/Minerals.jsx
+++ src/pages/Home/VitaminProtin/Minerals/Minerals.jsx
@@ -1,9 +1,41 @@
-
+/* eslint-disable react/no-unescaped-entities */
+import mineralseImage from "../../../../assets/images/sixNutritions/Minerals.png";
 
 const Minerals = () => {
     return (
-        <div>
-            
+        <div className="bg-green-100 p-10 rounded-lg px-20 mt-10">
+            <div className="flex justify-between items-center gap-20">
+                <div className=" flex-1">
+                    <div>
+                        <h1 className="font-comicSans text-left font-semibold text-4xl">
+                            Minerals
+                        </h1>
+                        <p className="mt-3 font-comicSans text-justify text-lg">
+                            Minerals, the silent champions of nutrition, are
+                            indispensable for the body's structural integrity
+                            and optimal functioning. Dive into the mineral-rich
+                            bounty of foods such as leafy greens, nuts, and
+                            seeds, offering essential nutrients like calcium for
+                            bone health and iron for oxygen transport. Explore
+                            the vital roles of minerals like potassium in
+                            maintaining fluid balance and zinc in supporting
+                            immune function. Uncover the hidden treasures in
+                            whole grains, legumes, and seafood, ensuring a
+                            diverse array of minerals to fortify the body.
+                            Embrace the harmonious dance of these microscopic
+                            heroes, contributing to the foundation of health and
+                            vitality.
+                        </p>
+                    </div>
+                </div>
+                <div className=" flex-1">
+                    <img
+                        className="w-96 ml-28 border-l-4  border-green-800 pl-10"
+                        src={mineralseImage}
+                        alt=""
+                    />
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/VitaminProtin/Vitamins/Vitamins.jsx
+++ src/pages/Home/VitaminProtin/Vitamins/Vitamins.jsx
@@ -1,26 +1,39 @@
 /* eslint-disable react/no-unescaped-entities */
-import vitaminsImage from '../../../../assets/images/sixNutritions/vitamin.png'
+import vitaminsImage from "../../../../assets/images/sixNutritions/vitamin.png";
 
 const Vitamins = () => {
     return (
         <div className="bg-green-100 p-10 rounded-lg px-28 mt-10">
-        <div className="flex justify-between items-center">
-            <div className="flex-1">
-                <img className="w-80" src={vitaminsImage} alt="" />
-            </div>
+            <div className="flex justify-between items-center">
+                <div className="flex-1">
+                    <img className="w-80" src={vitaminsImage} alt="" />
+                </div>
 
-            <div className="border-l-4 pl-10 border-green-800 flex-1">
-                <div>
-                    <h1 className="font-comicSans text-left font-semibold text-4xl">
-                        Vitamins
-                    </h1>
-                    <p className="mt-3 font-comicSans text-justify text-lg">
-                    Vitamins, the micronutrient powerhouses, are essential for a myriad of physiological processes that contribute to overall health and well-being. Explore the colorful world of fruits and vegetables, rich in vitamins like A, C, and K, supporting immune function and promoting skin health. Dive into dairy products for vitamin D, crucial for bone strength, and embrace the benefits of vitamin E found in nuts and seeds, acting as a potent antioxidant. Uncover the intricate roles of each vitamin, understanding how they synergistically contribute to the body's vitality and resilience. A diverse, nutrient-rich diet ensures a harmonious supply of these essential vitamins, promoting a life of wellness and vibrancy.
-                    </p>
+                <div className="border-l-4 pl-10 border-green-800 flex-1">
+                    <div>
+                        <h1 className="font-comicSans text-left font-semibold text-4xl">
+                            Vitamins
+                        </h1>
+                        <p className="mt-3 font-comicSans text-justify text-lg">
+                            Vitamins, the micronutrient powerhouses, are
+                            essential for a myriad of physiological processes
+                            that contribute to overall health and well-being.
+                            Explore the colorful world of fruits and vegetables,
+                            rich in vitamins like A, C, and K, supporting immune
+                            function and promoting skin health. Dive into dairy
+                            products for vitamin D, crucial for bone strength,
+                            and embrace the benefits of vitamin E found in nuts
+                            and seeds, acting as a potent antioxidant. Uncover
+                            the intricate roles of each vitamin, understanding
+                            how they synergistically contribute to the body's
+                            vitality and resilience. A diverse, nutrient-rich
+                            diet ensures a harmonious supply of these essential
+                            vitamins, promoting a life of wellness and vibrancy.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 };
 

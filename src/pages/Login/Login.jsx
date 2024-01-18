0bbf820e5d98beed737049d91405bd46ff424dc6
isalim079 <salimreza1025@gmail.com>
2024-01-18 21:23:34 +0600

    navbar title and logo added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,18 @@
 
+import logo from "/logo.png"
 
 const Login = () => {
     return (
-        <div>
-            This is login page
+        <div className="h-screen max-w-screen-2xl mx-auto">
+           <div>
+           <div className="flex justify-center flex-col items-center">
+                <img className="w-16" src={logo} alt="" />
+                <h1  className="text-4xl font-semibold">Welcome Back</h1>
+            </div>
+            <div>
+                <input type="text" name="email" id="" className="border border-green-200 rounded-md pl-4 h-12 font-normal font-comicSans min-w-[320px]" placeholder="Email" />
+            </div>
+           </div>
         </div>
     );
 };

7cd67452c05aad0e324181b65e56057695e08ed8
isalim079 <salimreza1025@gmail.com>
2024-01-20 15:39:07 +0600

    social login added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,10 @@
 
+import SocialLogin from "./SocialLogin";
 import logo from "/logo.png"
 
 const Login = () => {
     return (
-        <div className="h-screen max-w-screen-2xl mx-auto flex items-center justify-center">
+        <div className="h-[calc((100vh)-72px)] max-w-screen-2xl mx-auto flex items-center justify-center">
            <div>
            <div className="flex justify-center flex-col items-center text-green-900">
                 <img className="w-16" src={logo} alt="" />
@@ -14,7 +15,7 @@ const Login = () => {
                 <input type="text" name="email" id="" className="border border-green-200 rounded-md pl-4 h-12 font-normal font-comicSans min-w-[320px] block focus:outline-none focus:border-2 focus:border-green-400" placeholder="Email" />
                 <input type="password" name="password" id="" className="border border-green-200 rounded-md pl-4 h-12 font-normal font-comicSans min-w-[320px] block mt-4 focus:outline-none focus:border-2 focus:border-green-400" placeholder="Password" />
                 <div className=""><button className="bg-green-400 hover:bg-green-600 min-w-[320px] py-3 text-white mt-4 rounded-md">Login</button></div>
-                
+                <SocialLogin />
             </div>
            </div>
         </div>
--- src/pages/Login/SocialLogin.jsx
+++ src/pages/Login/SocialLogin.jsx
@@ -1,9 +1,11 @@
-
+import { FaGoogle } from "react-icons/fa";
+import { SiGithub } from "react-icons/si";
 
 const SocialLogin = () => {
     return (
-        <div>
-            
+        <div className="flex text-4xl text-green-900 space-x-8 mt-4">
+            <FaGoogle className="border-b-2 border-green-900 rounded-full p-1 transition-all duration-100 hover:rounded-none" />
+            <SiGithub className="border-b-2 border-green-900 rounded-full p-1 transition-all duration-100 hover:rounded-none" />
         </div>
     );
 };

ac97cb9d92afd6fba7dc22f868d9d98822e9b03e
isalim079 <salimreza1025@gmail.com>
2024-01-21 20:43:27 +0600

    animation added on hover social icon login page

--- src/pages/Login/SocialLogin.jsx
+++ src/pages/Login/SocialLogin.jsx
@@ -4,8 +4,8 @@ import { SiGithub } from "react-icons/si";
 const SocialLogin = () => {
     return (
         <div className="flex text-4xl text-green-900 space-x-8 mt-4">
-            <FaGoogle className="border-b-2 border-green-900 rounded-full p-1 transition-all duration-100 hover:rounded-none" />
-            <SiGithub className="border-b-2 border-green-900 rounded-full p-1 transition-all duration-100 hover:rounded-none" />
+            <FaGoogle className="border-b-2 border-green-900 rounded-full p-1 hover:rounded-none transition-all ease-in-out duration-100" />
+            <SiGithub className="border-b-2 border-green-900 rounded-full p-1 hover:rounded-none transition-all ease-in-out duration-100" />
         </div>
     );
 };

c56dbe995a4f234e6fd9de9fa466d402086d8750
isalim079 <salimreza1025@gmail.com>
2024-02-03 08:17:38 +0600

    tailwind config edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,21 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        Poppins: "'Poppins', sans-serif",
-        Lato: "'Lato', sans-serif",
-        comicSans: "'Comic Neue', cursive",
-      },
-      backgroundImage: {
-        carbohydratesBackground: "url('./src/assets/images/sixNutritions/carbohydrates.jpg')",
-        homePageBg: "url('./src/assets/images/homePageBg/homePageBg.png')"
-      }
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                Poppins: "'Poppins', sans-serif",
+                Lato: "'Lato', sans-serif",
+                comicSans: "'Comic Neue', cursive",
+            },
+            backgroundImage: {
+                carbohydratesBackground:
+                    "url('./src/assets/images/sixNutritions/carbohydrates.jpg')",
+                homePageBg:
+                    "url('./src/assets/images/homePageBg/homePageBg.png')",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 });
-

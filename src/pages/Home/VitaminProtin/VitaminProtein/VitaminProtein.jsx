569b3895e63d6257ab73559a84a422a4ed526feb
isalim079 <salimreza1025@gmail.com>
2024-02-05 08:37:16 +0600

    vitaminProtein section edited done

--- src/pages/Home/VitaminProtin/VitaminProtein/VitaminProtein.jsx
+++ src/pages/Home/VitaminProtin/VitaminProtein/VitaminProtein.jsx
@@ -9,20 +9,21 @@ const VitaminProtein = () => {
     return (
         <div className="bg-light-green-50 pb-5 md:pb-10">
             <div className="bg-homePageBg">
-            <div className="max-w-screen-2xl mx-auto">
-                <h1 className="text-center md:pt-20 mb-5 md:mb-14 md:text-5xl text-2xl font-semibold">
-                    Essential <span className="text-green-700">Nutrients</span>
-                </h1>
-                <div>
-                    <Protein />
-                    <Carbohydrates />
-                    <Fats />
-                    <Vitamins />
-                    <Minerals />
-                    <Water />
+                <div className="max-w-screen-2xl mx-auto">
+                    <h1 className="text-center md:pt-20 mb-5 md:mb-14 md:text-5xl text-2xl font-semibold">
+                        Essential{" "}
+                        <span className="text-green-700">Nutrients</span>
+                    </h1>
+                    <div>
+                        <Protein />
+                        <Carbohydrates />
+                        <Fats />
+                        <Vitamins />
+                        <Minerals />
+                        <Water />
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 };

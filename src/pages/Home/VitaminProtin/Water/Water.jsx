aeca20c0aa1dcf4ac8458b1a9b8535a42856202f
isalim079 <salimreza1025@gmail.com>
2024-02-09 10:05:15 +0600

    water section alignment updated

--- src/pages/Home/VitaminProtin/Water/Water.jsx
+++ src/pages/Home/VitaminProtin/Water/Water.jsx
@@ -5,7 +5,6 @@ import classNames from "classnames";
 import Aos from "aos";
 
 const Water = () => {
-
     useEffect(() => {
         Aos.init({
             duration: 1000,
@@ -15,7 +14,10 @@ const Water = () => {
     const animation = classNames("animate__animated", "animate__bounce");
 
     return (
-        <div className="bg-green-100 p-4 md:p-10 rounded-lg md:px-28 mt-10" data-aos="fade-left">
+        <div
+            className="bg-green-100 p-4 md:p-10 rounded-lg md:px-28 mt-10"
+            data-aos="fade-left"
+        >
             <div className="flex flex-col md:flex-row justify-between items-center">
                 <div className="flex-1">
                     <img className="w-80" src={waterImage} alt="" />
@@ -23,7 +25,9 @@ const Water = () => {
 
                 <div className="md:border-l-4 md:pl-10 border-green-800 flex-1">
                     <div>
-                        <h1 className={`font-comicSans text-left font-semibold text-4xl ${animation}`}>
+                        <h1
+                            className={`font-comicSans text-left font-semibold text-4xl ${animation}`}
+                        >
                             Waters
                         </h1>
                         <p className="mt-3 font-comicSans text-justify text-lg">

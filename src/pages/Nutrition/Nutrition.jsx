1da1a66491d8e327c67d8817c9be43e4b12ee5bd
isalim079 <salimreza1025@gmail.com>
2024-04-08 09:36:51 +0600

    react-hook-form added

--- src/pages/Nutrition/Nutrition.jsx
+++ src/pages/Nutrition/Nutrition.jsx
@@ -1,9 +1,30 @@
-
+import { useForm } from "react-hook-form";
 
 const Nutrition = () => {
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = async (data) => {};
+
     return (
         <div>
-            Nutritio
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <p>Protein</p>
+                    <input
+                        type="text"
+                        name="protein"
+                        id="protein"
+                        className="border border-green-200 rounded-md pl-10 h-12 font-normal font-comicSans min-w-[320px] block focus:outline-none focus:border-2 focus:border-green-400"
+                        placeholder="100gm"
+                        {...register("protein")}
+                    />
+                </div>
+            </form>
         </div>
     );
 };

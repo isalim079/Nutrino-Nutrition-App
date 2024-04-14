861a73488f5802921af55cf9892bfbb20567b2a1
isalim079 <salimreza1025@gmail.com>
2024-04-14 19:35:47 +0600

    bmi text added

--- src/pages/Nutrition/BMICalculator/BMICalculator.jsx
+++ src/pages/Nutrition/BMICalculator/BMICalculator.jsx
@@ -9,22 +9,25 @@ const BMICalculator = () => {
                     <p>{"<"} 18.5</p>
                 </div>
                 <div className="bg-green-700 text-white text-center py-1">
-                    <p>Underweight</p>
-                    <p>{"<"} 18.5</p>
+                    <p>Healthy</p>
+                    <p>18.5 - 24.9</p>
                 </div>
                 <div className="bg-yellow-700 text-white text-center py-1">
-                    <p>Underweight</p>
-                    <p>{"<"} 18.5</p>
+                    <p>Overweight</p>
+                    <p>25 - 29.9</p>
                 </div>
                 <div className="bg-orange-700 text-white text-center py-1">
-                    <p>Underweight</p>
-                    <p>{"<"} 18.5</p>
+                    <p>Obese</p>
+                    <p>30 - 39.9</p>
                 </div>
                 <div className="bg-red-700 text-white text-center py-1">
-                    <p>Underweight</p>
-                    <p>{"<"} 18.5</p>
+                    <p>Severely Obese</p>
+                    <p> ≥ 40 </p>
                 </div>
                
+            </div>
+            <div className="flex justify-center items-center mt-5">
+                <h3 className="text-xl flex  items-center gap-4">Your BMI is: <span className="font-bold text-4xl">0</span></h3>
             </div>
         </div>
     );
